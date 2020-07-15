f302e9c0d49fca1eda2dcf66838c2eb42c60af7c
Shapon Sheikh <shsheikhbd@gmail.com>
2020-07-15 19:46:33 +0600

    broken the homepage into smaller components, finished styling the menu-item

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [{
+                title: 'hats', 
+                imageUrl: 'https://i.ibb.co/Xk5S0Qy/hats-fedora-hat-manufacture-stack.png',
+                id: 1
+            },
+            {
+                title: 'jackets', 
+                imageUrl: 'https://i.ibb.co/zb35b18/pexels-photo-996329.png',
+                id: 2
+            },
+            {
+                title: 'sneakers', 
+                imageUrl: 'https://i.ibb.co/0Md5JKz/pexels-photo-322207.png',
+                id: 3
+            },
+            {
+                title: 'womens', 
+                imageUrl: 'https://i.ibb.co/kJQtGN2/pexels-photo-934070.png',
+                size: 'large',
+                id: 4
+            },
+            {
+                title: 'mens', 
+                imageUrl: 'https://i.ibb.co/StnLwxy/pexels-photo-325876.png',
+                size: 'large',
+                id: 5
+            }]
+        }
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {this.state.sections.map(({ title, imageUrl, size, id }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
